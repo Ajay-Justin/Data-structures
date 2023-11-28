bc23072e6d2ff5ac8d5edd4a543a20be5038e3d4
Ajay-Justin <back3me3up@gmail.com>
2023-11-28 21:47:53 +0530

    Basic opertaion of link list

--- linklist.c
+++ linklist.c
@@ -1,123 +1,328 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
-/* define structure for  node*/
+   
 struct lnode{
     int data;
     struct lnode *next;
 };
-
-/*we canot type like this in evry sentance struct lnode *node so we use typdef it easy to acces*/
-
+  
 typedef struct lnode node;
 node* head=NULL;
+  
+ 
+//function declaration
+void insertatBegging(int val);
+void Display(); 
+void insertatPostion(int pos, int val);
+void deleteatBegging();
+void deleteatPostion(int pos);
+int checkInvalidOrNOt(int pos);
+void insertatEnd(int val);
+void deleteatEnd();
+void readAtpostion(int pos);
+void insertAtEnd(int val);
+void search(int val);
+void total();
 
-/*Function Declaration*/
-
-void insert_at_start(int val);
-void Display();
-void insert_at_pos(int pos, int val);
-
-int main(){
-        int val,choice,pos;
+int main(){     
+    int val,choice,pos,ser;
+      
     while(1){
-        printf("----------------------option--------------------------\n");
-        printf("1.) Insert Begening\n");
-        printf("2.) Display\n");
-        printf("3.) Insert at postion\n");
-        printf("4.) Remove\n");
-        printf("---------------------------------------------------------\n");
         printf("\n");
-
-        printf("Enter the choice: ");
+        printf("__________________OPTIONS_____________________\n");
+        printf("1.Insert at begging\n");
+        printf("2.Insert at postion\n");
+        printf("3.Delete at postion\n");
+        printf("4.Delete at begging\n");
+        printf("5 Delete at end\n");
+        printf("6.Insert at end\n");
+        printf("7.Read at postion\n");
+        printf("8.search\n");
+        printf("9.Display data\n");
+        printf("10.Total ammount of data\n");
+        printf("___________________________________________\n");
+        printf("\n");
+        printf("Enter the Choice: ");
         scanf("%d",&choice);
-        printf("\n");
-        switch(choice){
-            case 1:printf("Enter the value: ");
-                   scanf("%d",&val);
-                   insert_at_start(val);
-                   break;
+        switch (choice){
 
-            case 2:
-                   Display();
-                   break;
+            case 1: printf("Inserted at postion\n");
+                    printf("Enter the value : ");
+                    scanf("%d",&val);
+                    insertatBegging(val);
+                    break;
+            case 2: printf("Inserted at postion\n");
+                    printf("Enter the postion : ");
+                    scanf("%d",&pos);
+                    printf("Enter the value : ");
+                    scanf("%d",&val);
+                    if(pos == 0)
+                        insertatBegging(val);
+                    if(pos < 0){
+                        printf("Invalid postion\n");
+                        return 0;
+                    }else{
+                        insertatPostion(pos, val);
+                    }
+                    break;
 
-            case 3:printf("Enter the Postion : ");
-                   scanf("%d",&pos);
-                   printf("Enter the value : ");
-                   scanf("%d",&val);
-                   if(pos == 0)
-                       insert_at_start(val);
-
-                   if(pos < 0){
-                       printf("Invalide option");
+            case 3: printf("Delete at postion\n");
+                    printf("Enter the postion : ");
+                    scanf("%d",&pos);
+                    if(pos < 0){
+                        printf("Invalid postion");
+                        return 0;
+                    }
+                    if(pos == 0){
+                        deleteatBegging();
+                        return 0;
+                    }
+                    deleteatPostion(pos);
+                    break;
+            case 4:printf("Delete at begging\n");
+                   if((checkInvalidOrNOt(pos))==0)
                        return 0;
-                   }
-                   insert_at_pos(pos, val);
-                   break;
 
+                    deleteatBegging();
+                    printf("\n");
+                    break;
 
+            case 5: printf("Delete at end\n");
+                    deleteatEnd();
+                    break;
+
+            case 6: printf("Inserted at end\n");
+                    printf("Enter the value : ");
+                    scanf("%d",&val);
+                    insertAtEnd(val);
+                    break;
+            case 7: printf("Read at postion\n");
+                    printf("Enter the postion : ");
+                    scanf("%d",&pos);
+                    if((checkInvalidOrNOt(pos))==0)
+                        return 0;
+                    readAtpostion(pos);
+                    break;
+            case 8: printf("search\n");
+                    printf("Enter the value : ");
+                    scanf("%d",&val);
+                    search(val);
+                    break;
+            case 9: Display();
+                    break;
+            case 10: printf("Total amount of data\n");
+                     total();
+                     break;
         }
-
     }
-}
-
-
-void insert_at_start(int val){
-  node* newnode = (node*) malloc(sizeof(node)); // type castin  malloc Null pointer into struct ptr
-  /*if newnode Get some error it will alocate NULL  this error is uncommon if we use lareg amount of data This error Ocurr*/
-  if(newnode == NULL){
-    printf("Out of Memory\n");
-    return;
+  
+  
   }
-  newnode->data=val;//assing value to the data for new node
-  if(head == NULL){
-    printf("Value inserted at Head ponter \n");
-    newnode->next=NULL;         // we assing NULL to Next pointer because of Ther is no value int next node it the head os the node so we assing NULL
-    head=newnode; // changing our newnode into head node;
-  }else{
-    newnode->next=head; // assing ptr next addr  value in head
-    head=newnode;
-  }
-
-  printf("Value inserted  %d\n",val);
-  printf("\n");
-}
-
-
-void Display(){
-    node* temp=head;
-    if(head == NULL){
-        printf("Ther is No value in the list\n");
+  
+ void insertatBegging(int val){
+    node* newnode=(node*)malloc(sizeof(node));
+    if(newnode == NULL){
+        printf("Out of Memory\n");
         return;
     }
+    newnode->data=val;
+    if(head == NULL){
+        printf("Data inserted at Head\n");
+        newnode->next=NULL;
+        head=newnode;
+        return;
+    }
+    newnode->next=head;
+    head=newnode;
+    printf("Data Inserted\n");
+}
+
+void Display(){
+
+    if(head == NULL){
+        printf("List is Empty\n");
+        return;
+    }
+    node* temp;
     temp=head;
-    printf("Values in the list : ");
+    printf("Values are in the list : ");
     while(temp != NULL){
         printf("%d, ",temp->data);
         temp=temp->next;
     }
     printf("\n");
+
 }
 
-void insert_at_pos(int pos,int val){
-    //create new node for new value
-    node* newnode = (node*)malloc(sizeof(node));
+void insertatPostion(int pos, int val){
+    node* newnode=(node*)malloc(sizeof(node));
     if(newnode == NULL){
         printf("Out of memory\n");
         return;
     }
 
     newnode->data=val;
-    node* temp=head;
-
+    node* temp;
+    temp=head;
     for(int i=0; i< pos-1; i++){
         temp=temp->next;
+        if(temp == NULL){
+            printf("Invlaid postion");
+            return;
+        }
+        printf("\n");
     }
     newnode->next=temp->next;
     temp->next=newnode;
-
-    printf("Inserted at %d postion, value %d ",pos, val);
+    printf("Data inserted %d at %d postion",val,pos);
     printf("\n");
+
+}
+void deleteatBegging(int pos,int val){
+    node* temp;
+    temp=head;
+    if(head == NULL){
+        printf("List is Empty\n");
+        return;
+    }
+    head=temp->next;
+    if(temp == NULL){
+        printf("Thier is no value\n");
+        return;
+    }
+    printf("Value Deleted at head %d\n",temp->data);
+    free(temp);
+    printf("\n");
+}
+
+void deleteatPostion(int pos){
+    node* prevnode;
+    node* temp=head;
+    if(head == NULL){
+        printf("List is Empty\n");
+        return;
+    }
+    
+    for(int i=1;i<=pos;i++){
+        prevnode=temp;
+        temp=temp->next;
+        if(temp == NULL){
+            printf("Invalid postion or no value\n");
+            return;
+        }
+        
+    }
+    prevnode->next=temp->next;
+    printf("Node delted at %d postion value  %d\n",pos,temp->data);
+    free(temp);
+    printf("\n");
+}
+
+int checkInvalidOrNOt(int pos){
+    if(pos >= 0){
+        return 1;
+    }
+    printf("Invalid postion\n");
+    printf("\n");
+    return 0;
+
+}
+void deleteatEnd(){
+    node* temp=head;
+    node* prevnode;
+    node* temp2;
+    if(head == NULL){
+        printf("List is empty\n");
+        return;
+    }
+    int i=0;
+    while(temp != NULL){
+        prevnode=temp2;
+        temp2=temp;
+        temp=temp->next;
+    }
+    
+        
+    printf("Value delete at end %d\n",temp2->data);
+    prevnode->next=NULL;
+    free(temp);
+    free(temp2);
+    printf("\n");
+    
+}
+
+void readAtpostion(int pos){
+    node *temp=head;
+    if(head == NULL){
+        printf("List is empty\n");
+        return;
+    }
+    for(int i=1;i<=pos;i++){
+        temp=temp->next;
+        if(temp == NULL){
+            printf("No value in thr given function\n");
+            return;
+        }
+    }
+    printf("The value of you enterd postion value  %d\n",temp->data);
+    printf("\n");
+    free(temp);
+}
+
+void insertAtEnd(int val){
+    node * newnode=(node*)malloc(sizeof(node));
+    node* prevnode;
+    node* temp;
+    if(head == NULL){
+        printf("List is empty\n");
+        return;
+    }
+    temp=head;
+    newnode->data=val;
+    while(temp != NULL){
+        prevnode=temp;
+        temp=temp->next;
+    }
+    prevnode->next=newnode;
+    newnode->next=NULL;
+    printf(" Data in prevnode : %d\n",prevnode->data);
+    printf("Data inserted %d",val);
+    free(temp);
+
+}
+
+void search(int val){
+    node* temp;
+    node* prevnode;
+    temp=head;
+    if(head == NULL){
+        printf("List is empty\n");
+        return;
+    }
+    int i=0;
+    while(temp != NULL){
+        prevnode=temp;
+        temp=temp->next;
+        
+        if(prevnode->data == val){
+            printf("Value found %d\n",prevnode->data);
+        }
+    }
+}
+
+void total(){
+    node* temp;
+    if(head == NULL){
+        printf("List is Empty\n");
+        return;
+    }
+    temp=head;
+    int i=0;
+    while(temp != NULL){
+        temp=temp->next;
+        i++;
+    }
+    printf("Total number of data in list %d",i);
+    free(temp);
 }
